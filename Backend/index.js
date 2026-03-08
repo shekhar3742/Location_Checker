edee85fddaf55c399a3eae47f4ff53778f664d28
shekhar3742 <imderek3742@gmail.com>
2026-03-08 16:27:35 +0530

    exact lat and long

--- Backend/index.js
+++ Backend/index.js
@@ -7,10 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const SITE_LOCATION = {
-  latitude: 28.630027432315067,
-  longitude: 77.37719558188438,
-};
-
+  latitude: 28.629363361585177,
+  longitude: 77.37706978560367,
+}; 
 const MAX_RADIUS = 20;
 
 // Haversine formula
