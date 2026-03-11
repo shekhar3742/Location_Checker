7e16f12a6ba11b379b44357e2124ca2be662e650
shekhar3742 <imderek3742@gmail.com>
2026-03-11 15:23:01 +0530

    office desk location

--- Backend/index.js
+++ Backend/index.js
@@ -7,8 +7,8 @@ app.use(cors());
 app.use(express.json());
 
 const SITE_LOCATION = {
-  latitude: 28.601244435263194,
-  longitude: 77.42941919674747,
+  latitude: 28.60113165,
+  longitude: 77.43221285,
 };
  
 const MAX_RADIUS = 20;
